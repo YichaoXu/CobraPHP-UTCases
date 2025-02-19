f366072f04d2ddde9d56a3377efc439cffc0b2b0
Yichao Xu <yxu166@jh.edu>
2025-02-18 20:50:19 -0500

    refactor: remove dynamic include examples and add new implementations for autoloading classes

--- patterns/78_autoloading_classes/main.php
+++ patterns/78_autoloading_classes/main.php
@@ -5,11 +5,3 @@ spl_autoload_register(function ($class_name) {
 });
 $a = $_GET["p1"];
 $obj  = new MyClass1($a);
-
-class MyClass1
-{
-    public function __construct($b)
-    {
-        echo $b;
-    }
-}
